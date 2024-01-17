7d64bdb789662b5ea74a0fc8b41a74cd3804d5e8
ojiwon99 <dhwldnjs0420@naver.com>
2024-01-17 16:53:59 +0900

    2024-01-07

--- src/com/javaex/ex04/PhoneBookApp.java
+++ src/com/javaex/ex04/PhoneBookApp.java
@@ -1,11 +1,15 @@
 package com.javaex.ex04;
 
+import java.io.FileReader;
 
 public class PhoneBookApp {
 
 	public static void main(String[] args) {
 		
 		//쓰기 빨대 쓰면 파일 다 지워짐
+		
+		//Reader fr = new FileReader(null)
+		
 		
 		//파일에서 읽은 내용을 리스트로 관리
 		//(이름,핸드폰,회사)-->person
